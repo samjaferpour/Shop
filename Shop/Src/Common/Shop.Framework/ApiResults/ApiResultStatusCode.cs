ef0288827af25481a2822a9046769014cc3e8768
s.jaferpour <s.jaferpour@JAFERPOUR-PC>
2023-01-07 11:59:17 +0330

    write ApiResultStatusCode code

--- Shop/Src/Common/Shop.Framework/ApiResults/ApiResultStatusCode.cs
+++ Shop/Src/Common/Shop.Framework/ApiResults/ApiResultStatusCode.cs
@@ -8,5 +8,12 @@ namespace Shop.Framework.ApiResults
 {
     public enum ApiResultStatusCode
     {
+        Success = 0,
+        ServerError = 1,
+        BadRequest = 2,
+        NotFound = 3,
+        UnAuthorized = 4,
+        EmptyList = 5,
+        LogicError = 6,
     }
 }

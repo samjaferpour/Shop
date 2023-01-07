6bae77b067f10679ebd09d2b628fbdaeb1ad1417
s.jaferpour <s.jaferpour@JAFERPOUR-PC>
2023-01-07 12:11:46 +0330

    write ApiResult code

--- Shop/Src/Common/Shop.Framework/ApiResults/ApiResult.cs
+++ Shop/Src/Common/Shop.Framework/ApiResults/ApiResult.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,32 @@ namespace Shop.Framework.ApiResults
 {
     public class ApiResult
     {
+        public bool IsSuccess { get; set; }
+        public ApiResultStatusCode StatusCode { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public ApiResult(bool isSuccess, ApiResultStatusCode statusCode, string message = null!)
+        {
+            IsSuccess = isSuccess;
+            StatusCode = statusCode;
+            Message = message;
+        }
+        #region Implicit Operators
+        public static implicit operator ApiResult(OkResult result)
+        {
+            return new ApiResult(true, ApiResultStatusCode.Success);
+        }
+        public static implicit operator ApiResult(ContentResult result)
+        {
+            return new ApiResult(true, ApiResultStatusCode.Success, result.Content);
+        }
+        public static implicit operator ApiResult(NotFoundResult result)
+        {
+            return new ApiResult(false, ApiResultStatusCode.NotFound);
+        }
+        public static implicit operator(BadRequestResult result)
+        {
+            return new ApiResult(false, ApiResultStatusCode.BadRequest);
+        }
+        #endregion
     }
 }

b4cfef625fd2c4e1bebaaf2c79f6ed666229c437
s.jaferpour <s.jaferpour@JAFERPOUR-PC>
2023-01-07 14:40:58 +0330

    add Settings to Dtos

---
+++ Shop/Src/Core/Shop.Contract/Dtos/Settings/SiteSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Contract.Dtos.Settings
+{
+    public class SiteSettings
+    {
+        public JwtConfig JwtConfig { get; set; } = null!;
+    }
+}
